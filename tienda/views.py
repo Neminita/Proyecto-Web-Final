60b569c59a10f302d3fa792a715d4106608f8a88
Jenemina <jeneby.lopez.cisternas@gmail.com>
2024-07-16 19:24:10 -0400

    pagination completa, color pendiente

--- tienda/views.py
+++ tienda/views.py
@@ -5,6 +5,8 @@ from .models import Clasica
 from .models import Rock
 from .forms import ViniloForm
 from django.contrib import messages
+from django.core.paginator import Paginator
+from django.http import Http404
 
 
 # Create your views here.
@@ -57,7 +59,19 @@ def agregar_producto(request):
 
 def listar_producto(request):
     vinilos = Vinilo.objects.all()
-    data = {"vinilos" : vinilos}
+    page = request.GET.get('page', 1)
+
+    try:
+        paginator = Paginator(vinilos, 5)
+        vinilos = paginator.page(page)
+    except:
+        raise Http404    
+
+    data = {
+        "entity" : vinilos,
+        'paginator': paginator
+            
+    }
     
     return render(request, 'tienda/vinilo/listar.html', data)
 
