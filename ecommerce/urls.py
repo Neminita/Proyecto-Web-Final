fb8b343ba16aea4bd4933727aa8dd4e6844d0582
Jenemina <jeneby.lopez.cisternas@gmail.com>
2024-07-16 23:06:11 -0400

    login

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -21,7 +21,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('tienda.urls'))
+    path('', include('tienda.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    
 ]
 
 if settings.DEBUG:
